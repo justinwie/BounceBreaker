4e6718dfda2713f63900baac7566ba362bec8b1f
Justin Wie <justinwie@Justins-MacBook-Pro.local>
2016-08-25 19:17:50 -0400

    Write 1.0 of game

--- bundle.js
+++ bundle.js
@@ -46,8 +46,8 @@
 
 	document.addEventListener("DOMContentLoaded", function(){
 	  var canvas = document.getElementById("myCanvas");
-	    canvas.width = 480;
-	    canvas.height = 320;
+	  canvas.width = 480;
+	  canvas.height = 480;
 	
 	  document.addEventListener("keydown", keyDownHandler, false);
 	  document.addEventListener("keyup", keyUpHandler, false);
@@ -60,11 +60,20 @@
 	  var lineX = canvas.width/2;
 	  var lineY = 0;
 	
-	  var speedScale = 7;
+	  var rightBorder = Math.floor(canvas.width * 0.8);
+	  var leftBorder = Math.floor(canvas.width * 0.2);
+	  var bottomBorder = Math.floor((canvas.height * 0.5));
+	  var topBorder = Math.floor(canvas.height * 0.8);
+	
+	  var randX = Math.floor(Math.random() * (rightBorder - leftBorder + 1)) + leftBorder;
+	  var randY = Math.floor(Math.random() * (topBorder - bottomBorder + 1)) + bottomBorder;
+	
+	  var speedScale = 15;
 	
 	  var dx = (lineX - x)/(y - lineY) * speedScale;
 	  var dy = -1 * speedScale;
 	
+	  var rightLevel = 10;
 	  var ballRadius = 10;
 	
 	  var rightPressed = false;
@@ -78,19 +87,20 @@
 	  var brickWidth = 60;
 	  var brickHeight = 30;
 	  var brickPadding = 2;
-	  var brickOffsetTop = 30;
-	  // var brickOffsetLeft = 0;
+	  var brickOffsetTop = brickHeight*1.5;
 	  var brickOffsetLeft = (canvas.width - ((brickWidth+(brickPadding))*brickColumnCount))/2;
 	
-	  var minNewBricks = 1;
+	  var minNewBricks = 4;
 	  var maxNewBricks = 6;
 	
-	  var score = 0;
+	  let ballLevel = 1;
+	
 	  var round = 1;
 	
 	  let newRound = false;
 	
-	  let randomNum = Math.floor(Math.random() * (maxNewBricks - minNewBricks + 1)) + minNewBricks;
+	  let powerBalls = [];
+	  let powerCounter = 0;
 	
 	  let existingBricks = new Array(brickColumnCount);
 	
@@ -102,6 +112,7 @@
 	  };
 	
 	  let newBricks = [];
+	
 	  function createNewBricks(){
 	    let randomNumBricks = Math.floor(Math.random() * (maxNewBricks - minNewBricks + 1)) + minNewBricks;
 	    for (var i = 0; i < randomNumBricks; i++) {
@@ -144,22 +155,41 @@
 	    for(i = 0; i < existingBricks.length; i++) {
 	      for(j = 0; j < existingBricks[i].length; j++) {
 	        let existingBrick = existingBricks[i][j];
-	        // if(existingBrick.hit === 0){
 	        if(existingBrick.power > 0){
-	          if((y+ballRadius/2 > existingBrick.y) && (y-ballRadius/2 < (existingBrick.y+brickHeight)) && (x+ballRadius/2 > existingBrick.x) && (x-ballRadius/2 < (existingBrick.x + brickWidth))){
-	          // if((y > existingBrick.y) && (y < (existingBrick.y+brickHeight)) && (x > existingBrick.x) && (x < (existingBrick.x + brickWidth))){
-	          // if((y+ballRadius > existingBrick.y-brickHeight) && (y-ballRadius < (existingBrick.y+brickHeight)) && (x+ballRadius > existingBrick.x-brickWidth) && (x-ballRadius < (existingBrick.x + brickWidth))){
-	            // if (Math.random()>0) {
-	            //   dx = -dx;
-	            // }
-	            existingBrick.power -= 1;
-	            if (Math.random()>0.95) {
-	              dx *= 1.1;
-	            }
-	            dy = -dy;
-	            // existingBrick.hit = 1;
-	            score++;
+	          let top = (y+ballRadius >= existingBrick.y);
+	          let bottom = (y-ballRadius <= (existingBrick.y+brickHeight));
+	          let left = (x+ballRadius >= existingBrick.x);
+	          let right = (x-ballRadius <= (existingBrick.x + brickWidth));
+	
+	          if((x+(ballRadius*2) >= existingBrick.x) && (x+(ballRadius*2) < (existingBrick.x + brickWidth)) && (y >= (existingBrick.y)) && (y+(2*ballRadius) <= existingBrick.y+brickHeight)){
+	            // left
+	            dx = -dx;
+	            existingBrick.power -= ballLevel;
 	          }
+	          else if((x <= existingBrick.x+ brickWidth) && (x > (existingBrick.x)) && (y >= (existingBrick.y)) && (y+(2*ballRadius) <= existingBrick.y+brickHeight)){
+	            // right
+	            dx = -dx;
+	            existingBrick.power -= ballLevel;
+	          }
+	          else if((y <= (existingBrick.y+brickHeight)) && (y > existingBrick.y) && (x >= existingBrick.x) && (x+(2*ballRadius) <= (existingBrick.x + brickWidth))){
+	            // bottom
+	            dy = -dy;
+	            existingBrick.power -= ballLevel;
+	
+	          }
+	          else if((y+(2*ballRadius) >= (existingBrick.y)) && (y+(2*ballRadius) < existingBrick.y+brickHeight) && (x >= existingBrick.x) && (x+(2*ballRadius) <= (existingBrick.x + brickWidth))){
+	            dy = -dy;
+	            existingBrick.power -= ballLevel;
+	          }
+	        }
+	      }
+	    }
+	    for(i = 0; i < powerBalls.length; i++) {
+	      let powerBall = powerBalls[i];
+	      if (powerBall.hit === 0) {
+	        if ((x+ballRadius > powerBall.x-powerBall.radius) && (x-ballRadius < powerBall.x+powerBall.radius) && (y+ballRadius > powerBall.y-powerBall.radius) && (y-ballRadius < powerBall.y+powerBall.radius)){
+	          ballLevel += powerBall.power;
+	          powerBall.hit = 1;
 	        }
 	      }
 	    }
@@ -169,7 +199,6 @@
 	    for(i = 0; i < existingBricks.length; i++) {
 	      for(j = 0; j < existingBricks[i].length; j++) {
 	        var brick = existingBricks[i][j];
-	        // if(brick.hit === 0){
 	        if(brick.power > 0){
 	          if(brick.y > canvas.height-brickHeight){
 	            alert("GAME OVER");
@@ -180,35 +209,9 @@
 	    }
 	  }
 	
-	  // function animateBricks(duration){
-	  //   // let accel = 5;
-	  //   // const brickDy = 50 + accel;
-	  //   // let start = new Date().getTime();
-	  //   // let end = start + duration;
-	  //
-	  //   let animation = function(){
-	  //     for(i = 0; i < existingBricks.length; i++) {
-	  //       for(j = 0; j < existingBricks[i].length; j++) {
-	  //         if(existingBricks[i][j].hit === 0){
-	  //           ctx.beginPath();
-	  //           ctx.rect(existingBricks[i][j].x, existingBricks[i][j].y, brickWidth, brickHeight);
-	  //           ctx.fillStyle = "Purple";
-	  //           ctx.fill();
-	  //           ctx.closePath();
-	  //         }
-	  //       }
-	  //     }
-	  //
-	  //     let animate = true;
-	  //     if (animate) requestAnimationFrame(animation)
-	  //   }
-	  //
-	  //   setTimeout(function(){ animation() }, 1)
-	  //   let animate = false
-	  // }
-	
 	  function drawArrow(){
 	    ctx.beginPath();
+	    ctx.setLineDash([10]);
 	    ctx.moveTo(x, y);
 	    ctx.lineTo(lineX, lineY);
 	    ctx.stroke();
@@ -217,15 +220,20 @@
 	  function drawBall(){
 	    ctx.beginPath();
 	    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	    ctx.fillStyle = "Green";
+	    ctx.fillStyle = '#69D2E7';
 	    ctx.fill();
 	    ctx.closePath();
+	  }
+	
+	  function drawPowerLevel(){
+	    ctx.font = "14px Helvetica";
+	    ctx.fillStyle = "Black";
+	    ctx.fillText('Power: '+ballLevel, 8, canvas.height-10);
 	  }
 	
 	  function createInitialBricks(){
 	    for(i = 0; i < existingBricks.length; i++) {
 	      for(j = 0; j < existingBricks[i].length; j++) {
-	        // if(existingBricks[i][j].hit === 0){
 	        if(existingBricks[i][j].power > 0){
 	            var brickX = (i*(brickWidth+brickPadding))+brickOffsetLeft;
 	            var brickY = (j*(brickHeight+brickPadding))+brickOffsetTop;
@@ -239,44 +247,96 @@
 	  function drawBricks() {
 	    for(i = 0; i < existingBricks.length; i++) {
 	      for(j = 0; j < existingBricks[i].length; j++) {
-	        // if(existingBricks[i][j].hit === 0){
 	        if(existingBricks[i][j].power > 0){
 	          ctx.beginPath();
 	          ctx.rect(existingBricks[i][j].x, existingBricks[i][j].y, brickWidth, brickHeight);
 	
-	          if (existingBricks[i][j].power > 5 && existingBricks[i][j].power < 10) {
-	            ctx.fillStyle = "Orange";
-	          }else if (existingBricks[i][j].power > 10) {
-	            ctx.fillStyle = "Red";
-	          } else {
-	            ctx.fillStyle = "Blue";
+	          if (existingBricks[i][j].power - ballLevel >= 5) {
+	            ctx.fillStyle = "#48b2ff";
+	          }
+	          else if ((existingBricks[i][j].power - ballLevel >= 3)&&(existingBricks[i][j].power - ballLevel < 5)) {
+	            ctx.fillStyle = "#48b2ff";
+	          }
+	          else if ((existingBricks[i][j].power - ballLevel >= 1)&&(existingBricks[i][j].power - ballLevel < 3)) {
+	            ctx.fillStyle = "#fa3f00";
+	          }
+	          else {
+	            ctx.fillStyle = "#ff862e";
 	          }
 	          ctx.fill();
 	          ctx.closePath();
-	
-	          ctx.font = "14px Arial";
+	          let fontAdjust = 3;
+	          if (existingBricks[i][j].power < 10) {
+	            fontAdjust = 2;
+	          }
+	          ctx.font = "14px Helvetica";
 	          ctx.fillStyle = "White";
-	          ctx.fillText(existingBricks[i][j].power, existingBricks[i][j].x+brickWidth/2-brickPadding*2, existingBricks[i][j].y+brickPadding*2+brickHeight/2);
+	          ctx.fillText(existingBricks[i][j].power, existingBricks[i][j].x+brickWidth/2-brickPadding*fontAdjust, existingBricks[i][j].y+brickPadding*2+brickHeight/2);
 	        }
 	      }
 	    }
 	  }
 	
 	  function drawScore(){
-	    ctx.font = "16px Arial";
+	    ctx.font = "16px Helvetica";
 	    ctx.fillStyle = "#0095DD";
 	    ctx.fillText("Score: "+score, 8, 20);
 	  }
 	
+	  function createPowerBall(){
+	    if (powerCounter < round-1) {
+	      randX = Math.floor(Math.random() * (rightBorder - leftBorder + 1)) + leftBorder;
+	      randY = Math.floor(Math.random() * (topBorder - bottomBorder + 1)) + bottomBorder;
+	
+	      powerBall = { x: randX, y: randY, hit: 0, power: 1, radius: 12, speed: 1 }
+	
+	      powerBalls.push(powerBall)
+	
+	      if (Math.random()>0.7) {
+	        randX = Math.floor(Math.random() * (rightBorder - leftBorder + 1)) + leftBorder;
+	        randY = Math.floor(Math.random() * (topBorder - bottomBorder + 1)) + bottomBorder;
+	        if (Math.random()>0.8) {
+	          powerBall = { x: randX, y: randY, hit: 0, power: +2, radius: 12, speed: 1 }
+	        }else {
+	          powerBall = { x: randX, y: randY, hit: 0, power: -1, radius: 15, speed: 1  }
+	        }
+	        powerBalls.push(powerBall)
+	      }
+	      powerCounter += 1;
+	    }
+	  }
+	
+	  function drawPowerBalls(){
+	    for(i = 0; i < powerBalls.length; i++) {
+	      let powerBall = powerBalls[i];
+	      if (powerBall.hit === 0) {
+	        let powerBallColor = powerBall.power === 1 ? "#bedc27" : "#dc27be"
+	        if (powerBall.power > 1) powerBallColor = "Black"
+	        let powerBallText = (powerBall.power >= 0) ? "+" : " !"
+	        let powerBallFont = (powerBall.power === -1) ? "18px" : "18px"
+	
+	        ctx.beginPath();
+	        ctx.arc(powerBall.x, powerBall.y, powerBall.radius, 0, Math.PI*2);
+	        ctx.fillStyle = powerBallColor;
+	        ctx.fill();
+	        ctx.closePath();
+	
+	        ctx.font = powerBallFont + " Helvetica";
+	        ctx.fillStyle = "White";
+	        ctx.fillText(powerBallText, powerBall.x-powerBall.radius/2, powerBall.y+powerBall.radius/2);
+	      }
+	    }
+	  }
+	
 	  function drawRound(){
-	    ctx.font = "14px Arial";
+	    ctx.font = "14px Helvetica";
 	    ctx.fillStyle = "Black";
 	    ctx.fillText("Round: "+round, 8, 20);
 	  }
 	
 	  function fillCanvas(){
 	    ctx.rect(0, 0, canvas.width, canvas.height);
-	    ctx.fillStyle = "lightgrey";
+	    ctx.fillStyle = "#f0f2e6";
 	    ctx.fill();
 	  }
 	
@@ -284,22 +344,26 @@
 	    fillCanvas()
 	    checkIfLost();
 	
-	    drawRound();
 	    drawBricks();
-	    drawBall();
+	    drawPowerBalls()
 	
 	    if (startRound === 0) {
 	      drawArrow();
+	      createPowerBall()
 	    }
+	
+	    drawRound();
+	    drawBall();
+	    drawPowerLevel()
 	
 	    collisionDetection();
 	
 	    if (startRound != 0) {
 	      if(x + dx > (canvas.width-ballRadius) || (x + dx) < ballRadius) {
-	        dx = -dx;
+	        dx *= -1;
 	      }
 	      if((y + dy) < ballRadius) {
-	        dy = -dy;
+	        dy *= -1;
 	      }
 	      x += dx;
 	      y += dy;
@@ -327,8 +391,13 @@
 	          existingBricks[randomCol].unshift(newBricks[i][0])
 	        }
 	
-	        newBricks = [];
+	        for (var i = 0; i < powerBalls.length; i++) {
+	          if (powerBalls[i].power != 1) {
+	            powerBalls.splice(i, 1)
+	          }
+	        }
 	
+	        newBricks = [];
 	        startRound = 0;
 	      }
 	    }
@@ -336,14 +405,14 @@
 	    if (rightPressed && startRound === 0) {
 	      if (lineX < canvas.width) {
 	        if (lineY === 0) {
-	          lineX += 20
+	          lineX += 25
 	        }
 	        else{
-	          lineY -= 20
+	          lineY -= 25
 	        }
 	      }
 	      else {
-	        lineY += 20;
+	        lineY += 25;
 	      }
 	
 	      dx = (lineX - x)/(y - lineY) * speedScale;
@@ -353,14 +422,14 @@
 	    else if (leftPressed && startRound === 0) {
 	      if (lineX > 0) {
 	        if (lineY != 0) {
-	          lineY -= 20
+	          lineY -= 25
 	        }
 	        else {
-	          lineX -= 20
+	          lineX -= 25
 	        }
 	      }
 	      else {
-	        lineY += 20
+	        lineY += 25
 	      }
 	
 	      dx = (lineX - x)/(y - lineY) * speedScale;
@@ -369,6 +438,7 @@
 	
 	    if (spacePressed) {
 	      startRound++;
+	
 	    }
 	  }
 	
@@ -382,14 +452,6 @@
 	  game();
 	
 	});
-	
-	// if (existingBricks[i][j].y > 150 && existingBricks[i][j].y < 230) {
-	//   ctx.fillStyle = "Orange";
-	// }else if (existingBricks[i][j].y > 230) {
-	//   ctx.fillStyle = "Red";
-	// } else {
-	//   ctx.fillStyle = "Blue";
-	// }
 
 
 /***/ }
